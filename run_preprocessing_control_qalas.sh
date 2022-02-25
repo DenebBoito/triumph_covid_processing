f22ccf29e6d5a2ba683cad280451c03f7ebc7b06
DenebBoito <deneb.boito@gmail.com>
2022-02-25 14:17:21 +0100

    changes in control preproc & registration

---
+++ run_preprocessing_control_qalas.sh
@@ -0,0 +1,64 @@
+# #!/bin/bash
+
+# Loop over all CONTROL subjects & do the preprocessing
+
+# steps:
+#  - motion correction
+#  - anatomical brain extraction
+#  - compute matrix for registration dwi2anatomical
+#
+# Here we use the T1 from QALAS (qMRI)
+#
+# Updated 11/02/2022
+
+
+
+for i in /home/denbo78/test_folder_covid_preproc/c* ; do
+
+    cd $i
+    echo "Processing" $i
+    
+    # make a temporary directory for analysis
+    mkdir analysis_qalas
+
+    # copy the mdd_long & anatomical to this folder
+    cp MDDlong/mdd_long.nii analysis_qalas
+    cp *QALAS*/*e3*.nii analysis_qalas
+
+    # copy the bvals & bvecs into the analysis directory
+    cp -a /home/denbo78/Triumph_Git/mddLong_experimental_params/. analysis_qalas
+
+    # now can work in the new folder
+    cd analysis_qalas 
+
+    # run the motion correction using mrtrix
+    dwifslpreproc mdd_long.nii mdd_mc.mif -rpe_none -pe_dir AP -eddy_options "--data_is_shelled --slm=linear" -fslgrad bvecs.bvecs bvals.bvals
+
+    # convert the mc data into nii & extract the rotated bvec & bval
+    mrconvert mdd_mc.mif mdd_eddy.nii.gz -export_grad_fsl eddybvecs.txt eddybvals.txt
+
+    # in case AFNI complains, we reorient the brain
+    # 3dWarp -deoblique -prefix deobliqued.nii sag*.nii 
+
+    # resample to convenient grid
+    3dresample -dxyz 0.5 0.5 0.5 -input *e3*.nii -prefix anat_resampled.nii
+
+    # run brain extraction with AFNI 
+    3dSkullStrip -input anat_resampled.nii -prefix skullstripped.nii -orig_vol -fill_hole 10
+
+    # create a mean B0 for subsequent registration to anatomical
+    dwiextract mdd_mc.mif - -bzero | mrmath - mean mean_bzero.mif -axis 3 
+    mrconvert mean_bzero.mif mean_bzero.nii
+
+    # create the mask to be used in QTI+?
+    dwi2mask mdd_mc.mif -| maskfilter - dilate -| mrconvert - mdd_mask.nii
+
+    # run the registration to get the transformation to be applied later
+    # for the controls, given that we have the T2 map, we need to run fast to get an appropriate wm segmentation
+    # fast -t 2 -n 3 -o seg skullstripped.nii
+    epi_reg --epi=mean_bzero.nii --t1=anat_resampled.nii --t1brain=skullstripped.nii --out=out_epi_reg #--wmseg=seg_pve_2.nii.gz
+    # this outputs a matrix file called out_epi_reg.mat which can be input to flirt to do the registration
+    # flirt -ref flirted.nii -in map.nii -applyxfm -init out_epi_reg.mat -out reg_map
+
+
+done
---
+++ run_registration_qti2anat_qalas.sh
@@ -0,0 +1,37 @@
+# #!/bin/bash
+
+# Loop over all subjects & do the registration of the QTI maps to anatomical image
+
+for i in /home/denbo78/test_folder_covid_preproc/c* ; do
+
+    cd $i
+    echo "Processing" $i
+
+    # enter the analysis folder
+    cd analysis_qalas
+
+    # enter the QTI+_Results folder
+    cd QTI+_Results
+
+    # create a new folder for the registered images
+    mkdir registered
+
+    # loop over the nifti files
+    for j in *.nii ; do
+
+    	# run the registration
+    	filename="${j##*/}"
+    	pre="reg_"
+        outname="$pre$filename"
+
+    	flirt -ref ../anat_resampled.nii -in $filename -applyxfm -init ../out_epi_reg.mat -out $outname
+
+    	# move to registered folder
+    	mv $outname* registered
+
+    done
+
+
+	
+
+done
